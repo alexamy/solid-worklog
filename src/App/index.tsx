054047980b9f91727ec6c6cf52c7d8e8c2fde7bd
Aleksandr Chashchin <ac@happynumbers.com>
2025-01-11 00:08:43 +0100

    use 6 id

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -627,7 +627,7 @@ function getDefaultStore(): Store {
 
 // methods
 function randomId() {
-  return Math.random().toString(16).substring(2, 6);
+  return Math.random().toString(16).substring(2, 8);
 }
 
 function toTimestamp(date: Date) {
