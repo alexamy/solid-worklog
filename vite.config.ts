1fd105967ed228d5bf6b2e8fb962d8f11897ce86
Aleksandr Chashchin <ac@happynumbers.com>
2025-01-10 18:31:21 +0100

    add custom name slug

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,21 @@ export default defineConfig({
   base: '/solid-worklog/',
   plugins: [
     solid(),
-    wyw({ displayName: true, preprocessor: 'none' }),
+    wyw({
+      preprocessor: 'none',
+      classNameSlug(hash, title, opts) {
+        const prefix = toStyleKebabCase(opts.name === 'index' ? opts.dir : opts.name);
+        const className = toStyleKebabCase(title);
+        return `${prefix}_${className}--${hash.substring(0, 4)}`;
+      },
+    }),
   ],
-})
+});
+
+export function toStyleKebabCase(str: string): string {
+  return str
+    .replace(/([A-Z]+)([A-Z][a-z])/g, '$1-$2')
+    .replace(/([a-z])([A-Z])/g, '$1-$2')
+    .toLowerCase()
+    .replace(/^s-/, '') // extra convention to allow `s` prefix
+}
