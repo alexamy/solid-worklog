5e055cb7c9815d9458700e6dfd198491b1e97d84
Aleksandr Chashchin <ac@happynumbers.com>
2025-01-11 15:00:59 +0100

    add eslint solid plugin

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,8 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import solid from "eslint-plugin-solid/configs/typescript";
+import * as tsParser from "@typescript-eslint/parser";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
@@ -8,4 +10,14 @@ export default [
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{ts,tsx}"],
+    ...solid,
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "tsconfig.app.json",
+      },
+    },
+  },
 ];
