e51ed3e53724de34aeaa3be1e692aee8669cde4e
Aleksandr Chashchin <ac@happynumbers.com>
2025-01-13 11:25:25 +0100

    ref

--- src/App/DatePicker/index.tsx
+++ src/App/DatePicker/index.tsx
@@ -93,9 +93,9 @@ function ThemeController() {
           d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
       </svg>
       <input
+        class="toggle theme-controller"
         type="checkbox"
         value="dark"
-        class="toggle theme-controller"
         checked={checked()}
         onChange={onThemeChange}
       />
