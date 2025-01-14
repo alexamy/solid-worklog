8e1088273db791278b84bcea61af74f1b482f8d6
Aleksandr Chashchin <ac@happynumbers.com>
2025-01-14 17:03:17 +0100

    add download

--- src/App/DatePicker/index.tsx
+++ src/App/DatePicker/index.tsx
@@ -127,13 +127,14 @@ function ThemeController() {
 function DownloadButton(props: { onClick: () => void }) {
   return (
     <svg
+      class='size-6 stroke-gray-600 hover:stroke-gray-300 cursor-pointer'
+      onClick={() => props.onClick()}
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
       viewBox="0 0 24 24"
       stroke-width="1.5"
       stroke="currentColor"
-      class="size-6"
-      onClick={() => props.onClick()}
+
     >
       <path stroke-linecap="round" stroke-linejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
     </svg>
@@ -148,8 +149,8 @@ function Cog(props: {
     <svg
       class="size-6 cursor-pointer"
       classList={{
-        'hover:stroke-gray-400 stroke-gray-600': !props.selected,
-        'hover:stroke-gray-400 stroke-primary': props.selected,
+        'hover:stroke-gray-300 stroke-gray-600': !props.selected,
+        'hover:stroke-gray-300 stroke-primary': props.selected,
       }}
       onClick={() => props.onClick()}
       xmlns="http://www.w3.org/2000/svg"
