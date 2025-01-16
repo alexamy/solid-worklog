62cabaeafaf19cc543057021b31856e9704cc93d
Aleksandr Chashchin <ac@happynumbers.com>
2025-01-16 21:51:05 +0100

    extract settings form

--- src/App/Settings/index.tsx
+++ src/App/Settings/index.tsx
@@ -2,6 +2,15 @@ import { useAppContext } from '../store/app';
 import { useDataContext } from '../store/data';
 
 export function Settings() {
+  return (
+    <div class="flex flex-col gap-8">
+      <SettingsForm />
+      <Utilities />
+    </div>
+  )
+}
+
+function SettingsForm() {
   const [appStore, setAppStore] = useAppContext();
 
   function setJiraHost(text: string) {
@@ -10,43 +19,40 @@ export function Settings() {
   }
 
   return (
-    <div class="flex flex-col gap-8">
-      <div class='flex flex-col gap-4'>
-        <div class="form-control w-fit">
-          <label class="label cursor-pointer p-0">
-            <input
-              class="checkbox mr-2"
-              id="skip-empty-days"
-              type="checkbox"
-              aria-label="Skip empty days"
-              checked={appStore.skipEmptyDays}
-              onInput={(e) => setAppStore('skipEmptyDays', e.target.checked)}
-            />
-            <span class="label-text"> Skip empty days in date picker</span>
-          </label>
-        </div>
-
-        <label class="form-control w-full max-w-md">
-          <div class="label">
-            <span class="label-text">Jira host</span>
-          </div>
+    <div class='flex flex-col gap-4'>
+      <div class="form-control w-fit">
+        <label class="label cursor-pointer p-0">
           <input
-            class="input input-bordered w-full max-w-md text-sm"
-            id="jira-host"
-            type="text"
-            aria-label="Jira host"
-            size={60}
-            value={appStore.jiraHost}
-            onInput={(e) => setJiraHost(e.target.value)}
+            class="checkbox mr-2"
+            id="skip-empty-days"
+            type="checkbox"
+            aria-label="Skip empty days"
+            checked={appStore.skipEmptyDays}
+            onInput={(e) => setAppStore('skipEmptyDays', e.target.checked)}
           />
+          <span class="label-text"> Skip empty days in date picker</span>
         </label>
       </div>
-      <Utilities />
+
+      <label class="form-control w-full max-w-md">
+        <div class="label">
+          <span class="label-text">Jira host</span>
+        </div>
+        <input
+          class="input input-bordered w-full max-w-md text-sm"
+          id="jira-host"
+          type="text"
+          aria-label="Jira host"
+          size={60}
+          value={appStore.jiraHost}
+          onInput={(e) => setJiraHost(e.target.value)}
+        />
+      </label>
     </div>
-  )
+  );
 }
 
-export function Utilities() {
+function Utilities() {
   const [_1, _2, {
     resetToDefault,
     resetEmpty,
